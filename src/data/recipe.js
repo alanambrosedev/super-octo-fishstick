cbdfaedd7865da92a7b0e0b00bd81713c216386f
Alan Ambrose <alanambrosedev@gmail.com>
2025-12-01 22:16:58 +0530

    Add recipe data and update App component to display recipes

---
+++ src/data/recipe.js
@@ -0,0 +1,16 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Pasta",
+    cookTime: 15,
+    ingredients: ["pasta", "tomato", "salt"],
+  },
+  {
+    id: 2,
+    title: "Cake",
+    cookTime: 20,
+    ingredients: ["flour", "sugar", "eggs"],
+  },
+];
+
+export default recipes;
